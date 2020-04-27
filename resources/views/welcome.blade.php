6fe52ce703f02df52fbf7f82a077c374e14ed40f
jhuei0831 <57594444+jhuei0831@users.noreply.github.com>
2020-04-27 22:22:28 +0800

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('_layouts.home.app')
+@section('content')
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                測試
+            </div>
+            <div class="card-body">
+                TEST
+            </div>
+        </div>
+    </div>
+@endsection
